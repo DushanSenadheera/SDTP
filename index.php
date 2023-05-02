605535cbb975627c9808b5ec4601bcc1c443f579
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-05-02 12:54:37 +0530

    added main file structure and header, footer section

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/index.css">
+    <link rel="stylesheet" href="styles/footer.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h2 class="logo">LOGO</h2>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Explore</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">contact</a></li>
+            </ul>
+            <a href=""><button class="primary-btn">Sign In</button></a>
+        </nav>
+        <div class="header-content">
+            <h3>Let's <span>create</span> <br> a <span>cleaner</span> future <span>together</span></h3>
+            <br>
+            <div class="description">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat quasi voluptatem cumque voluptatum! Labore, consequuntur dicta, odit sed et velit, culpa a assumenda at aperiam sunt voluptates veniam quibusdam hic?</p>
+            </div>
+            <div class="cta">
+                <a href=""><button class="primary-btn">Get to Know</button></a>
+            </div>
+        </div>
+    </header>
+    <main>
+        <!-- main content here -->
+    </main>
+        <footer>
+            <div class="footer-top">
+                <div class="footer-logo">
+                    <h2 class="logo">LOGO</h2>
+                </div>
+                <div class="footer-nav">
+                    <h4>Navigation</h4>
+                    <br>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Explore</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="footer-other-links">
+                    <h4>Other</h4>
+                    <br>
+                    <ul>
+                        <li><a href="#">Become a Memeber</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-contact">
+                    <h4>Get In Touch</h4>
+                    <br>
+                    <ul>
+                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                        <li><a href="#"><i class="fab fa-tiktok"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <br>
+            <hr>
+            <br>
+            <div class="footer-bottom">
+                <small>Designed & Developed By Group 22</small>
+            </div>
+        </footer>
+</body>
+</html>
